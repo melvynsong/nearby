aa7d138b9191c41cac40a209a98aacbcee2420e2
melvynsong <song.kg@gmail.com>
2026-04-21 21:19:16 +0800

    Remove legacy app/showcase/[key] route to fix showcase detail routing

--- app/showcase/[key]/loading.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import SiteLoadingState from '@/components/SiteLoadingState';
-
-export default function ShowcaseKeyLoading() {
-  return <SiteLoadingState context={{ page: 'showcase-key' }} />;
-}
