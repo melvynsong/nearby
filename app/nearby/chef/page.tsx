5c2cf7dbe25dfe8f62fa5164df7599bcb60815f5
melvynsong <song.kg@gmail.com>
2026-04-25 08:32:11 +0800

    Chef Dashboard

--- app/nearby/chef/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ChefPage() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-[#f5f6f8]">
-      <div className="text-3xl font-bold text-neutral-800 mb-4">We are cooking something, don't kan cheong</div>
-      <div className="text-6xl mb-6">🍳👨‍🍳</div>
-    </main>
-  );
-}
